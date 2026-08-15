a69fd03f44aaf5e65a4920481c7125477cb9503f
blebik <illyarszx87@gmail.com>
2026-08-15 15:42:35 +0200

    update slapple collect

--- autoSlapple.lua
+++ autoSlapple.lua
@@ -2,9 +2,12 @@ local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
 
 function inArena()
-   if not plr.Character:FindFirstChild("isInArena").Value then
-      plr.Character:PivotTo(CFrame.new(-1210,328,4))
+   if not plr.Character:FindFirstChild("isInArena").Value and not plr.Backpack:FindFirstChildOfClass("Tool") then
+      plr.Character:PivotTo(CFrame.new(-1210,330,4))
       task.wait(1)
+       if not plr.Character:FindFirstChild("isInArena").Value and not plr.Backpack:FindFirstChildOfClass("Tool") then 
+            inArena()
+       end
    end
 end
 
@@ -21,12 +24,10 @@ function CollectSlapple(obj)
       end
 end
 function sborslapov()
-   plr.Character.HumanoidRootPart.Anchored = true
    for i,slapple in pairs(game.Workspace.Arena.island5.Slapples:GetChildren()) do
      CollectSlapple(slapple)
    end
    plr.Character.HumanoidRootPart.Anchored = false
 end
-
 
 sborslapov()
