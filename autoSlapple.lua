cea2c8cf5bc0afad8c34abb5ffc567ffddae1194
blebik <illyarszx87@gmail.com>
2026-08-16 18:46:55 +0200

    auto tp

--- autoSlapple.lua
+++ autoSlapple.lua
@@ -1,5 +1,6 @@
 local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
+local TeleportService = game:GetService("TeleportService")
 
 function inArena()
    if not plr.Character:FindFirstChild("isInArena").Value and not plr.Backpack:FindFirstChildOfClass("Tool") then
@@ -10,6 +11,15 @@ function inArena()
        end
    end
 end
+
+
+
+function teleportToNewServer()
+    local placeId = game.PlaceId
+    print(placeId)
+    TeleportService:Teleport(placeId, plr)
+end
+
 
 function CollectSlapple(obj)
       inArena()
@@ -28,6 +38,9 @@ function sborslapov()
      CollectSlapple(slapple)
    end
    plr.Character.HumanoidRootPart.Anchored = false
+   task.wait(2)
+   teleportToNewServer()
 end
+
 
 sborslapov()
