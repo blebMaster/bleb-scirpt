79fa42b926782878ba552acc0bae7cf95266d0d6
blebik <illyarszx87@gmail.com>
2026-08-28 20:50:36 +0200

    Update autoSlapple.lua

--- autoSlapple.lua
+++ autoSlapple.lua
@@ -30,7 +30,7 @@ function Serverhop()
       local body = game:GetService("HttpService"):JSONDecode(req)
       if body and body.data then
          for i, v in next, body.data do
-            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers-1 and v.id ~= game.JobId then
                table.insert(servers, 1, v.id)
             end
          end
